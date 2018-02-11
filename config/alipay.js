b3d9c8cf7dbf67b7b7d045dcc658d338338e8a6e
wanghao8080 <117663444@qq.com>
2018-02-11 15:54:25 +0800

    配置文件：应用的密钥与支付宝公钥

--- config/alipay.js
+++ config/alipay.js
@@ -2,7 +2,19 @@
 
 const Env = use('Env')
 
+const privateKey =
+  '-----BEGIN RSA PRIVATE KEY-----\n' +
+  Env.get('ALIPAY_APP_PRIVATE_KEY') +
+  '\n-----END RSA PRIVATE KEY-----'
+
+const aliPayPublicKey =
+  '-----BEGIN PUBLIC KEY-----\n' +
+  Env.get('ALIPAY_PUBLIC_KEY') +
+  '\n-----END PUBLIC KEY-----'
+
 module.exports = {
+  privateKey,
+  aliPayPublicKey,
   app_id: Env.get('ALIPAY_APP_ID'),
   notify_url: Env.get('ALIPAY_NOTIFY_URL'),
   return_url: Env.get('ALIPAY_RETURN_URL'),
